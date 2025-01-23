4e44125e8f647db2a23a382fbc3f552cd0b96fb8
GoogGod <szverev362@gmail.com>
2025-01-23 20:49:43 +1000

    color update

--- main.py
+++ main.py
@@ -2,7 +2,26 @@ import pygame as pg
 
 pg.init()
 
+# RGB
+colors = {
+    "blue": (60, 80, 230),
+    "background": (255, 255, 255)
+}
+
 screen_width = 800
 screen_height = 600
 
 screen = pg.display.set_mode((screen_width, screen_height))
+pg.display.set_caption("My first game")
+
+running = True
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    screen.fill(colors["blue"])
+    pg.display.flip()
+    
+pg.quit()
